f139f33003533c7a10410a5d4709cc070ed499c9
Ritik <ritikkne@gmail.com>
2020-04-01 14:02:26 +0530

    Add install.sh, move scripts to bin

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp $(pwd)/bin/{SetAsDefault,SetAsFavourite,SetWallpaperNow} ~/.local/share/nautilus/scripts/
+cp $(pwd)/bin/{set-default-wallpaper,update-wallpaper} ~/.local/bin/
+
+sudo chmod +x ~/.local/share/nautilus/scripts/*
+sudo chmod +x ~/.local/bin/{set-default-wallpaper,update-wallpaper}
+
+mkdir -p ~/.cache/ShuffleWallpaper/{choices,default,tmp}
+
+exit 0
